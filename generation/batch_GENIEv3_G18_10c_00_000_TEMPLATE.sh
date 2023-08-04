12f3a1317b4ed5ceef8bf7362947e0f3fe8a15d9
Callum Wilkinson <cwilkinson@lbl.gov>
2023-08-04 15:22:20 -0700

    Commit production scripts

---
+++ generation/batch_GENIEv3_G18_10c_00_000_TEMPLATE.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#SBATCH --image=docker:wilkinsonnu/nuisance_project:genie_v3.2.0
+#SBATCH --qos=shared
+#SBATCH --constraint=cpu
+#SBATCH --time=720
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=4GB
+#SBATCH --module=cvmfs
+
+## These change for each job
+THIS_SEED=__THIS_SEED__
+FILE_NUM=__FILE_NUM__
+NU_PDG=__NU_PDG__
+OUTDIR=__OUTDIR__
+FLUX_FILE=__FLUX_FILE__
+FLUX_HIST=__FLUX_HIST__
+TARG=__TARG__
+ROOT_NAME=__ROOT_NAME__
+OUTFILE=__OUTFILE__
+
+## Output file name
+TUNE=G18_10c_00_000
+NEVENTS=1000000
+E_MIN=0.12
+E_MAX=__E_MAX__
+
+## Where to temporarily save files
+TEMPDIR=${SCRATCH}/${OUTFILE/.root/}_${THIS_SEED}
+
+echo "Moving to SCRATCH: ${TEMPDIR}"
+mkdir ${TEMPDIR}
+cd ${TEMPDIR}
+
+## Get the splines that are now needed...
+cp $CFS/dune/users/cwilk/MC_inputs/${TUNE}_v320_splines.xml .
+
+## Get the flux file
+cp $CFS/dune/users/cwilk/MC_inputs/${FLUX_FILE} .
+
+## Force it to use the CVMFS version that actually allows INCL...
+echo "Starting gevgen..."
+shifter -V ${PWD}:/output --entrypoint /bin/bash -c "source /cvmfs/fermilab.opensciencegrid.org/products/genie/bootstrap_genie_ups.sh; \
+        setup genie v3_02_00c -q e20:inclxx:prof; \
+        export INCL_SRC_DIR=/cvmfs/fermilab.opensciencegrid.org/products/genie/local/inclxx/v5_2_9_5a/source; \
+	gevgen -n ${NEVENTS} -t ${TARG} -p ${NU_PDG} \
+        --cross-sections ${TUNE}_v320_splines.xml \
+        --tune ${TUNE} --seed ${THIS_SEED} \
+        -f ${FLUX_FILE},${FLUX_HIST} -e ${E_MIN},${E_MAX} -o ${OUTFILE} &> /dev/null"
+
+echo "Starting PrepareGENIE..."
+shifter -V ${PWD}:/output --entrypoint PrepareGENIE -i $OUTFILE -f ${FLUX_FILE},${FLUX_HIST} \
+	-t $TARG -o ${OUTFILE/.root/_NUIS.root}
+
+shifter -V ${PWD}:/output --entrypoint nuisflat -f GenericVectors -i GENIE:${OUTFILE/.root/_NUIS.root} -o ${OUTFILE/.root/_NUISFLAT.root} -q "nuisflat_SaveSignalFlags=false"
+echo "Complete"
+
+## Copy back the important files
+cp ${TEMPDIR}/${OUTFILE/.root/_NUIS.root} ${OUTDIR}/.
+cp ${TEMPDIR}/${OUTFILE/.root/_NUISFLAT.root} ${OUTDIR}/.
+
+## Clean up
+rm -r ${TEMPDIR}
+
---
+++ generation/batch_submit_IOP_fluxes.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+## If I want to pile more on later
+FIRST_JOB=0
+LAST_JOB=4
+
+## Loop over these configs (max 15)
+FIRST_CONFIG=0
+LAST_CONFIG=23
+
+name_arr=( "T2KSK_osc_FHC_numu_H2O" "T2KSK_osc_RHC_numubar_H2O" \
+	   "T2KND_FHC_numu_C8H8" "T2KND_RHC_numubar_C8H8" \
+	   "T2KND_FHC_numu_H2O" "T2KND_RHC_numubar_H2O" \
+	   "DUNEND_FHC_numu_Ar40" "DUNEND_RHC_numubar_Ar40" \
+	   "DUNEFD_osc_FHC_numu_Ar40" "DUNEFD_osc_RHC_numubar_Ar40" \
+	   "flat_0-10GeV_numu_H2O" "flat_0-10GeV_numubar_H2O" \
+	   "flat_0-10GeV_numu_C8H8" "flat_0-10GeV_numubar_C8H8" \
+	   "flat_0-10GeV_numu_Ar40" "flat_0-10GeV_numubar_Ar40" \
+	   "flat_0-10GeV_nue_H2O" "flat_0-10GeV_nuebar_H2O" \
+           "flat_0-10GeV_nue_C8H8" "flat_0-10GeV_nuebar_C8H8" \
+           "flat_0-10GeV_nue_Ar40" "flat_0-10GeV_nuebar_Ar40" \
+	   "NuMIME_FHC_numu_Ar40" "NuMIME_RHC_numubar_Ar40" )
+
+nu_pdg_arr=( 14 -14 \
+	     14 -14 \
+	     14 -14 \
+	     14 -14 \
+	     14 -14 \
+	     14 -14 \
+	     14 -14 \
+             14 -14 \
+	     12 -12 \
+	     12 -12 \
+	     12 -12 \
+	     14 -14 \
+	   )
+
+emax_arr=( 10 10 \
+           10 10 \
+	   10 10 \
+	   50 50 \
+	   50 50 \
+	   10 10 \
+	   10 10 \
+	   10 10 \
+	   10 10 \
+           10 10 \
+           10 10 \
+	   50 50 \
+	 )
+
+targ_arr=( "1000080160[0.8889],1000010010[0.1111]" "1000080160[0.8889],1000010010[0.1111]" \
+	   "1000060120[0.9231],1000010010[0.0769]" "1000060120[0.9231],1000010010[0.0769]" \
+ 	   "1000080160[0.8889],1000010010[0.1111]" "1000080160[0.8889],1000010010[0.1111]" \
+	   "1000180400[1.00]" "1000180400[1.00]" \
+	   "1000180400[1.00]" "1000180400[1.00]" \
+	   "1000080160[0.8889],1000010010[0.1111]" "1000080160[0.8889],1000010010[0.1111]" \
+           "1000060120[0.9231],1000010010[0.0769]" "1000060120[0.9231],1000010010[0.0769]" \
+	   "1000180400[1.00]" "1000180400[1.00]" \
+	   "1000080160[0.8889],1000010010[0.1111]" "1000080160[0.8889],1000010010[0.1111]" \
+           "1000060120[0.9231],1000010010[0.0769]" "1000060120[0.9231],1000010010[0.0769]" \
+           "1000180400[1.00]" "1000180400[1.00]" \
+	   "1000180400[1.00]" "1000180400[1.00]" )
+
+flux_file_arr=( "T2KSK_FHC_NuFit5NO.root" "T2KSK_RHC_NuFit5NO.root" \
+		"t2kflux_2016_plus250kA.root" "t2kflux_2016_minus250kA.root" \
+		"t2kflux_2016_plus250kA.root" "t2kflux_2016_minus250kA.root" \
+		"DUNE_OptimizedEngineeredNov2017_REGULAR.root" "DUNE_OptimizedEngineeredNov2017_REGULAR.root" \
+		"DUNE_numu_osc_NuFit5NO.root" "DUNE_numu_osc_NuFit5NO.root" \
+		"flat_flux_10GeV.root" "flat_flux_10GeV.root" \
+		"flat_flux_10GeV.root" "flat_flux_10GeV.root" \
+		"flat_flux_10GeV.root" "flat_flux_10GeV.root" \
+		"flat_flux_10GeV.root" "flat_flux_10GeV.root" \
+                "flat_flux_10GeV.root" "flat_flux_10GeV.root" \
+                "flat_flux_10GeV.root" "flat_flux_10GeV.root" \
+		"MINERvA_flux_ME1F.root" "MINERvA_flux_ME5ARHC.root" )
+
+flux_hist_arr=( "sk_numu_osc" "sk_numubar_osc" \
+		"enu_nd280_numu" "enu_nd280_numub" \
+		"enu_nd280_numu" "enu_nd280_numub" \
+		"numu_NDFHC_flux" "numubar_NDRHC_flux" \
+		"numu_FDFHC_flux_osc" "numubar_FDRHC_flux_osc" \
+		"flat_flux" "flat_flux" \
+	        "flat_flux" "flat_flux" \
+                "flat_flux" "flat_flux" \
+		"flat_flux" "flat_flux" \
+                "flat_flux" "flat_flux" \
+                "flat_flux" "flat_flux" \
+		"flux_E_cvweighted_CV_WithStatErr" "flux_E_cvweighted_CV_WithStatErr" )
+
+## Loop over templates
+for GENERATOR in GENIEv3_G18_10X_00_000 GENIEv3_G18_10a_00_000 GENIEv3_G18_10b_00_000 \
+		 GENIEv3_G18_10c_00_000 GENIEv3_CRPA21_04a_00_000 GENIEv3_G21_11a_00_000 \
+		 NUWRO_LFGRPA NUWRO_SF NEUT562 GENIEv3_AR23_20i_00_000
+do
+    for i in $(seq ${FIRST_CONFIG} ${LAST_CONFIG}); do
+	
+	OUTDIR="${CFS}/dune/users/cwilk/MC_IOP_review/${GENERATOR}"
+	
+	if [ ! -d "${OUTDIR}" ]; then
+	    mkdir -p ${OUTDIR}
+	fi
+	
+	TEMPLATE=batch_${GENERATOR}_TEMPLATE.sh
+
+	for JOB in $(seq ${FIRST_JOB} ${LAST_JOB}); do
+	    
+	    printf -v PADJOB "%04d" $JOB
+	    
+	    OUTFILE="${name_arr[$i]}_${GENERATOR}_1M_${PADJOB}.root"
+	    
+	    ## Check if file has already been processed
+	    if [ -f "${OUTDIR}/${OUTFILE/.root/_NUISFLAT.root}" ]; then
+                continue
+	    fi
+	    echo "Processing ${OUTFILE}"
+	    
+	    ## Copy the template
+	    THIS_TEMP=${TEMPLATE/_TEMPLATE/_${name_arr[$i]}_${PADJOB}}
+	    cp ${TEMPLATE} ${THIS_TEMP}
+	    
+	    ## Set everything important...
+	    sed -i "s/__THIS_SEED__/${RANDOM}/g" ${THIS_TEMP}
+	    sed -i "s/__FILE_NUM__/${PADJOB}/g" ${THIS_TEMP}
+	    sed -i "s/__NU_PDG__/${nu_pdg_arr[$i]}/g" ${THIS_TEMP}
+	    sed -i "s/__OUTDIR__/${OUTDIR//\//\\/}/g" ${THIS_TEMP}
+	    sed -i "s/__OUTFILE__/${OUTFILE}/g" ${THIS_TEMP}
+	    sed -i "s/__FLUX_FILE__/${flux_file_arr[$i]}/g" ${THIS_TEMP}
+	    sed -i "s/__FLUX_HIST__/${flux_hist_arr[$i]}/g" ${THIS_TEMP}
+            sed -i "s/__TARG__/${targ_arr[$i]}/g" ${THIS_TEMP}
+            sed -i "s/__ROOT_NAME__/${name_arr[$i]}/g" ${THIS_TEMP}
+	    sed -i "s/__E_MAX__/${emax_arr[$i]}/g" ${THIS_TEMP}
+	    echo "Submitting ${THIS_TEMP}"
+	    
+	    ## Submit the template
+	    sbatch ${THIS_TEMP}
+	    
+	    ## No need to delete, so done
+	    rm ${THIS_TEMP}
+	done
+    done
+done
