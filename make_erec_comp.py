cca7e68605b65fecba2e8973276f2dd266f90a3b
Callum Wilkinson <cwilkinson@lbl.gov>
2025-03-24 22:58:28 -0700

    Update the Erec plots

--- make_erec_comp.py
+++ make_erec_comp.py
@@ -3,7 +3,12 @@ import os
 from ROOT import gStyle, TGaxis, TPad, TLine, gROOT, TH1, TColor, TCanvas, TFile, TH1D, gPad, TLegend, kWhite, gDirectory, gEnv
 from glob import glob
 
-from make_erec_comp_ECA import make_generator_comp_noratio
+from plotting_functions import make_generator_comp, make_generator_comp_noratio
+## def make_generator_comp(outPlotName, inFileList, nameList, colzList, \
+##                         plotVar="q0", binning="100,0,5", cut="cc==1", \
+##                         labels="q_{0} (GeV); d#sigma/dq_{0} (#times 10^{-38} cm^{2}/nucleon)",
+##                         legDim=[0.65, 0.5, 0.85, 0.93], ratLimits=[0.4, 1.6], maxVal=None, isShape=False)
+
 
 ## Use double precision for TTree draw
 gEnv.SetValue("Hist.Precision.1D", "double")
@@ -36,210 +41,7 @@ TGaxis.SetExponentOffset(-0.06, 0., "y")
 
 ## Make some colorblind friendly objects
 ## From: https://personal.sron.nl/~pault/#sec:qualitative
-kkBlue    = TColor(9000,   0/255., 119/255., 187/255.)
-kkCyan    = TColor(9001,  51/255., 187/255., 238/255.)
-kkTeal    = TColor(9002,   0/255., 153/255., 136/255.)
-kkOrange  = TColor(9003, 238/255., 119/255.,  51/255.)
-kkRed     = TColor(9004, 204/255.,  51/255.,  17/255.)
-kkMagenta = TColor(9005, 238/255.,  51/255., 119/255.)
-kkGray    = TColor(9006, 187/255., 187/255., 187/255.)
 
-can = TCanvas("can", "can", 600, 1000)
-can .cd()
-
-def get_chain(inputFileNames, max_files=999):
-
-    print("Found", inputFileNames)
-    inFile   = ROOT.TFile(glob(inputFileNames)[0], "READ")
-    inFlux   = None
-    inEvt    = None
-    treeName = None
-    nFiles   = 0
-
-    for key in inFile.GetListOfKeys():
-        if "FLUX" in key.GetName():
-            inFlux = inFile.Get(key.GetName())
-            inFlux .SetDirectory(0)
-        if "VARS" in key.GetName():
-            treeName = key.GetName()
-    
-    inFile .Close()
-    
-    inTree = ROOT.TChain(treeName)
-    old_number = 0
-    for inputFileName in glob(inputFileNames):
-        
-        inTree.Add(inputFileName)
-
-        if old_number == inTree.GetEntries():
-            print("Skipping", inputFileName, "as it has no events")
-            continue
-        old_number=inTree.GetEntries()
-        
-	nFiles += 1
-        if nFiles > max_files: break
-
-        
-        ## Add the histograms up
-        inFile   = ROOT.TFile(inputFileName, "READ")
-        for key in inFile.GetListOfKeys():
-            if "EVT" not in key.GetName(): continue
-            tempEvt = inFile.Get(key.GetName())
-            if not inEvt:
-                inEvt = tempEvt
-                inEvt .SetDirectory(0)
-            else: inEvt.Add(tempEvt)
-        inFile.Close()    
-
-    print("Found", inTree.GetEntries(), "events in chain")
-
-    return inTree, inFlux, inEvt, nFiles
-
-def make_generator_comp(outPlotName, inFileList, nameList, colzList, \
-                        plotVar="q0", binning="100,0,5", cut="cc==1", \
-                        labels="q_{0} (GeV); d#sigma/dq_{0} (#times 10^{-38} cm^{2}/nucleon)",
-                        isShape=False, maxVal=None):
-
-    ## Skip files that already exist
-    if os.path.isfile("plots/"+outPlotName):
-        print("Skipping plots/"+outPlotName, "which already exists!")
-        return
-    
-    isLog = False
-    histList = []
-    ratList  = []
-    
-    can     .cd()
-    top_pad = TPad("top_pad", "top_pad", 0, 0.4, 1, 1)
-    top_pad .Draw()
-    bot_pad = TPad("bot_pad", "bot_pad", 0, 0, 1, 0.4)
-    bot_pad .Draw()
-    top_pad .cd()
-    ratio = 0.6/0.4
-
-    titleSize = 0.05
-    labelSize = 0.04
-    
-    ## Loop over the input files and make the histograms
-    for inFileName in inFileList:
-
-        ## Modify to use glob
-        inTree, inFlux, inEvt, nFiles = get_chain(inFileName)
-        
-        inTree.Draw(plotVar+">>this_hist("+binning+")", "("+cut+")*fScaleFactor*1E38")
-        thisHist = gDirectory.Get("this_hist")
-        thisHist .SetDirectory(0)
-
-        ## Deal with different numbers of files
-        thisHist.Scale(1./nFiles, "width")
-
-        ## Allow for shape option
-        if isShape: thisHist .Scale(1/thisHist.Integral())
-
-        ## Retain for use
-        thisHist .SetNameTitle("thisHist", "thisHist;"+labels)
-        histList .append(thisHist)
-
-    ## Sort out the ratio hists
-    nomHist = histList[0].Clone()
-    nomHist .Rebin(2)
-    for hist in histList:
-        rat_hist = hist.Clone()
-        rat_hist .Rebin(2)
-        rat_hist .Divide(nomHist)
-        ratList  .append(rat_hist)
-        
-    ## Get the maximum value
-    if not maxVal:
-        maxVal   = 0
-        for hist in histList:
-            if hist.GetMaximum() > maxVal:
-                maxVal = hist.GetMaximum()        
-        maxVal = maxVal*1.1
-        
-    ## Actually draw the histograms
-    histList[0].Draw("HIST")
-    histList[0].SetMaximum(maxVal)
-
-    ## Unify title/label sizes
-    histList[0] .GetYaxis().SetTitleSize(titleSize)
-    histList[0] .GetYaxis().SetLabelSize(labelSize)
-    histList[0] .GetYaxis().SetTitleOffset(1.4)
-    
-    ## Suppress x axis title and labels
-    histList[0] .GetXaxis().SetTitle("")
-    histList[0] .GetXaxis().SetTitleSize(0.0)
-    histList[0] .GetXaxis().SetLabelSize(0.0)
-    
-    if not isLog: histList[0].SetMinimum(0)
-    for x in reversed(range(len(histList))):
-        histList[x].SetLineWidth(3)
-        histList[x].SetLineColor(colzList[x])
-        histList[x].Draw("HIST SAME")
-
-    
-    ## Now make a legend
-    dim = [0.2, 0.85, 0.98, 1.00]
-    leg = TLegend(dim[0], dim[1], dim[2], dim[3], "", "NDC")
-    leg .SetShadowColor(0)
-    leg .SetFillColor(0)
-    leg .SetLineWidth(0)
-    leg .SetTextSize(0.036)
-    leg .SetNColumns(3)
-    leg .SetLineColor(kWhite)
-    for hist in range(len(histList)):
-        leg .AddEntry(histList[hist], nameList[hist], "l")
-    leg .Draw("SAME")
-
-    gPad.SetLogy(0)
-    if isLog: gPad.SetLogy(1)
-    gPad.SetRightMargin(0.02)
-    gPad.SetTopMargin(0.15)
-    gPad.SetLeftMargin(0.15)
-    gPad.SetBottomMargin(0.022)
-    gPad.RedrawAxis()
-    gPad.Update()
-
-    ## Now ratios on the bottom panel
-    bot_pad.cd()
-
-    ## Skip ratList[0] as everything is a ratio w.r.t that
-    ratList[1] .Draw("][ HIST")
-    ratList[1] .SetMaximum(1.5)
-    ratList[1] .SetMinimum(0.5)
-
-    ratList[1] .GetYaxis().SetTitle("Ratio w.r.t. "+nameList[0])
-    ratList[1] .GetYaxis().CenterTitle(1)
-    ratList[1] .GetXaxis().CenterTitle(0)
-    ratList[1] .GetYaxis().SetTitleOffset(0.85)    
-    ratList[1] .GetXaxis().SetNdivisions(505)
-    ratList[1] .GetXaxis().SetTickLength(histList[0].GetXaxis().GetTickLength()*ratio)
-    ratList[1] .GetXaxis().SetTitleSize(titleSize*ratio)
-    ratList[1] .GetXaxis().SetLabelSize(labelSize*ratio)
-    ratList[1] .GetYaxis().SetTitleSize(titleSize*ratio)
-    ratList[1] .GetYaxis().SetLabelSize(labelSize*ratio)
-
-    for x in reversed(range(1, len(histList))):
-        ratList[x].SetLineWidth(3)
-        ratList[x].SetLineColor(colzList[x])
-        ratList[x].Draw("][ HIST SAME")
-
-    midline = TLine(ratList[1].GetXaxis().GetBinLowEdge(1), 1, ratList[1].GetXaxis().GetBinUpEdge(ratList[1].GetNbinsX()), 1)
-    midline .SetLineWidth(3)
-    midline .SetLineColor(ROOT.kBlack)
-    midline .SetLineStyle(11)
-    midline .Draw("LSAME")
-    
-    ## Save
-    gPad  .RedrawAxis()
-    gPad  .SetRightMargin(0.02)
-    gPad  .SetTopMargin(0.00)
-    gPad  .SetBottomMargin(0.25)
-    gPad  .SetLeftMargin(0.15)
-    can   .Update()
-    can .SaveAs("plots/"+outPlotName)
-
-    
 def make_T2K_erec_plots(inputDir="inputs/"):
 
     nameList = ["GENIE 10a",\
@@ -253,7 +55,7 @@ def make_T2K_erec_plots(inputDir="inputs/"):
     colzList = [9000, 9001, 9002, 9003, 9004, 9006, 9005]
     
     ## QE reco
-    qe_cut = "cc==1 && Sum$(abs(pdg) > 100 && abs(pdg) < 2000)==0 && Sum$(abs(pdg) > 2300 && abs(pdg) < 100000)==0"
+    qe_cut = "cc==1 && Sum$(abs(pdg) > 100 && abs(pdg) < 2000)==0 && Sum$(abs(pdg) > 2300 && abs(pdg) < 100000)==0 && Sum$(pdg==2212) > 0"
 
     ## Loop over configs
     for det in ["T2KND", "T2KSK_osc"]:
@@ -268,14 +70,14 @@ def make_T2K_erec_plots(inputDir="inputs/"):
                           inputDir+"/"+det+"_"+flux+"_H2O_NUWRO_LFGRPA_1M_*_NUISFLAT.root"\
                           ]
             
-            #make_generator_comp(det+"_"+flux+"_H2O_EnuQE_gencomp.png", inFileList, nameList, colzList, "Enu_QE", "80,0,2", qe_cut, \
-            #                    "E_{#nu}^{rec, QE} (GeV); d#sigma/dE_{#nu}^{rec, QE} (#times 10^{-38} cm^{2}/nucleon)", False)
+            make_generator_comp(det+"_"+flux+"_H2O_EnuQE_gencomp.pdf", inFileList, nameList, colzList, "Enu_QE", "80,0,2", qe_cut, \
+                                "E_{#nu}^{rec, QE} (GeV); d#sigma/dE_{#nu}^{rec, QE} (#times 10^{-38} cm^{2}/nucleon)")
 
-            make_generator_comp_noratio(det+"_"+flux+"_H2O_EnuQE_gencomp_noratio.png", inFileList, nameList, colzList, "Enu_QE", "80,0,2", qe_cut, \
-                                "E_{#nu}^{rec, QE} (GeV); d#sigma/dE_{#nu}^{rec, QE} (#times 10^{-38} cm^{2}/nucleon)", False)
+            #make_generator_comp_noratio(det+"_"+flux+"_H2O_EnuQE_gencomp_noratio.pdf", inFileList, nameList, colzList, "Enu_QE", "80,0,2", qe_cut, \
+            #                    "E_{#nu}^{rec, QE} (GeV); d#sigma/dE_{#nu}^{rec, QE} (#times 10^{-38} cm^{2}/nucleon)", False)
         
-            #make_generator_comp(det+"_"+flux+"_H2O_EnuQEbias_gencomp.png", inFileList, nameList, colzList, "(Enu_QE - Enu_true)/Enu_true", "80,-1,1", qe_cut, \
-            #                    "(E_{#nu}^{rec, QE} - E_{#nu}^{true})/E_{#nu}^{true}; Arb. norm.", True)
+            make_generator_comp(det+"_"+flux+"_H2O_EnuQEbias_gencomp.pdf", inFileList, nameList, colzList, "(Enu_QE - Enu_true)/Enu_true", "80,-1,1", qe_cut, \
+                                "(E_{#nu}^{rec, QE} - E_{#nu}^{true})/E_{#nu}^{true}; Arb. norm.", isShape=True)
             
 def make_DUNE_erec_plots(inputDir="inputs/"):
 
@@ -306,140 +108,18 @@ def make_DUNE_erec_plots(inputDir="inputs/"):
                           inputDir+"/"+det+"_"+flux+"_Ar40_NUWRO_LFGRPA_1M_*_NUISFLAT.root"\
                           ]
             
-            #make_generator_comp(det+"_"+flux+"_Ar40_Enurec_gencomp.png", inFileList, nameList, colzList, enuhad, "80,0,8", ehad_cut, \
-            #                    "E_{#nu}^{rec, had} (GeV); d#sigma/dE_{#nu}^{rec, had} (#times 10^{-38} cm^{2}/nucleon)", False)
+            make_generator_comp(det+"_"+flux+"_Ar40_Enurec_gencomp.pdf", inFileList, nameList, colzList, enuhad, "80,0,8", ehad_cut, \
+                                "E_{#nu}^{rec, had} (GeV); d#sigma/dE_{#nu}^{rec, had} (#times 10^{-38} cm^{2}/nucleon)")
 
-            make_generator_comp_noratio(det+"_"+flux+"_Ar40_Enurec_gencomp_noratio.png", inFileList, nameList, colzList, enuhad, "80,0,8", ehad_cut, \
-                                        "E_{#nu}^{rec, had} (GeV); d#sigma/dE_{#nu}^{rec, had} (#times 10^{-38} cm^{2}/nucleon)", False)
+            #make_generator_comp_noratio(det+"_"+flux+"_Ar40_Enurec_gencomp_noratio.pdf", inFileList, nameList, colzList, enuhad, "80,0,8", ehad_cut, \
+            #                            "E_{#nu}^{rec, had} (GeV); d#sigma/dE_{#nu}^{rec, had} (#times 10^{-38} cm^{2}/nucleon)", False)
 
-            #make_generator_comp(det+"_"+flux+"_Ar40_Enurecbias_gencomp.png", inFileList, nameList, colzList, "("+enuhad+" - Enu_true)/Enu_true", "80,-1,1", ehad_cut, \
-            #                    "(E_{#nu}^{rec, had} - E_{#nu}^{true})/E_{#nu}^{true}; Arb. norm.", True)
+            make_generator_comp(det+"_"+flux+"_Ar40_Enurecbias_gencomp.pdf", inFileList, nameList, colzList, "("+enuhad+" - Enu_true)/Enu_true", "80,-1,1", ehad_cut, \
+                                "(E_{#nu}^{rec, had} - E_{#nu}^{true})/E_{#nu}^{true}; Arb. norm.", isShape=True)
 
-def make_DUNE_erecoverq0_plots(inputDir="inputs/"):
-
-    nameList = ["GENIE 10a",\
-                "GENIE 10b",\
-                "GENIE 10c",\
-                "CRPA",\
-                "SuSAv2",\
-                "NEUT",\
-                "NuWro"\
-                ]
-    colzList = [9000, 9001, 9002, 9003, 9004, 9006, 9005]
-    
-    ## QE reco
-    cc_cut = "cc==1"
-    ehadrec = "Sum$((abs(pdg)==11 || (abs(pdg)>17 && abs(pdg)<2000))*E) + Sum$((abs(pdg)>2300 &&abs(pdg)<10000)*E) + Sum$((abs(pdg)==2212)*(E - sqrt(E*E - px*px - py*py - pz*pz)))"
-    ehadtrue ="Sum$((abs(pdg)==11 || (abs(pdg)>17 && abs(pdg)<2000))*E) + Sum$((abs(pdg)>2300 &&abs(pdg)<10000)*E) + Sum$((abs(pdg)==2212 || abs(pdg)==2112)*(E - sqrt(E*E - px*px - py*py - pz*pz)))"
-
-    eavail = "Sum$((abs(pdg)==11 || (abs(pdg)>17 && abs(pdg)<210) || (abs(pdg)>212 && abs(pdg)<2000)*E) + Sum$((abs(pdg)>2300 &&abs(pdg)<10000)*E) + Sum$((abs(pdg)==2212 || abs(pdg)==211)*(E - sqrt(E*E - px*px - py*py - pz*pz)))"
-
-    binning="50,0,1"
-    
-    ## Loop over configs
-    for det in ["DUNEND"]: #, "DUNEFD_osc"]:
-        for flux in ["FHC_numu", "RHC_numubar"]:
-            ## These files can be found here (no login required): https://portal.nersc.gov/project/dune/data/2x2/simulation
-            inFileList = [inputDir+"/"+det+"_"+flux+"_Ar40_GENIEv3_G18_10a_00_000_1M_*_NUISFLAT.root",\
-                          inputDir+"/"+det+"_"+flux+"_Ar40_GENIEv3_G18_10b_00_000_1M_*_NUISFLAT.root",\
-                          inputDir+"/"+det+"_"+flux+"_Ar40_GENIEv3_G18_10c_00_000_1M_*_NUISFLAT.root",\
-                          inputDir+"/"+det+"_"+flux+"_Ar40_GENIEv3_CRPA21_04a_00_000_1M_*_NUISFLAT.root",\
-                          inputDir+"/"+det+"_"+flux+"_Ar40_GENIEv3_G21_11a_00_000_1M_*_NUISFLAT.root",\
-                          inputDir+"/"+det+"_"+flux+"_Ar40_NEUT562_1M_*_NUISFLAT.root",\
-                          inputDir+"/"+det+"_"+flux+"_Ar40_NUWRO_LFGRPA_1M_*_NUISFLAT.root"\
-                          ]
-
-            make_generator_comp(det+"_"+flux+"_Ar40_ehadtrue_over_q0_gencomp.png", inFileList, nameList, colzList, ehadtrue, binning, cc_cut, \
-                                "E_{had}^{true}/q_{0}; d#sigma/#left(dE_{had}^{true}/q_{0}#right) (#times 10^{-38} cm^{2}/nucleon)", False)
-
-            make_generator_comp_noratio(det+"_"+flux+"_Ar40_ehadtrue_over_q0_gencomp_noratio.png", inFileList, nameList, colzList, ehadtrue, binning, cc_cut, \
-                                        "E_{had}^{true}/q_{0}; d#sigma/#left(dE_{had}^{true}/q_{0}#right) (#times 10^{-38} cm^{2}/nucleon)", False)
-            
-            make_generator_comp(det+"_"+flux+"_Ar40_ehadrec_over_q0_gencomp.png", inFileList, nameList, colzList, ehadrec, binning, cc_cut, \
-                                "E_{had}^{rec}/q_{0}; d#sigma/#left(dE_{had}^{rec}/q_{0}#right) (#times 10^{-38} cm^{2}/nucleon)", False)
-
-            make_generator_comp_noratio(det+"_"+flux+"_Ar40_ehadrec_over_q0_gencomp_noratio.png", inFileList, nameList, colzList, ehadrec, binning, cc_cut, \
-                                        "E_{had}^{rec}/q_{0}; d#sigma/#left(dE_{had}^{rec}/q_{0}#right) (#times 10^{-38} cm^{2}/nucleon)", False)
-
-            make_generator_comp(det+"_"+flux+"_Ar40_eavail_over_q0_gencomp.png", inFileList, nameList, colzList, eavail, binning, cc_cut, \
-                                "E_{avail}/q_{0}; d#sigma/#left(dE_{avail}/q_{0}#right) (#times 10^{-38} cm^{2}/nucleon)", False)
-            
-            make_generator_comp_noratio(det+"_"+flux+"_Ar40_eavail_over_q0_gencomp_noratio.png", inFileList, nameList, colzList, eavail, binning, cc_cut, \
-                                        "E_{avail}/q_{0}; d#sigma/#left(dE_{avail}/q_{0}#right) (#times 10^{-38} cm^{2}/nucleon)", False)
-            
-def make_FSI_erec_plots(inputDir="inputs/"):
-
-    nameList = ["GENIE 10a",\
-                "GENIE 10b",\
-                "GENIE 10c",\
-                "GENIE 10d"\
-                ]
-    colzList = [9000, 9001, 9002, 9003, 9004, 9006, 9005]
-    
-    ## QE reco
-    ehad_cut = "cc==1"
-
-    ## Loop over configs
-    for nu_pdg in [14, -14]:
-        for e_mono in [(1,0.5,1.2), (2,1,2.4), (3,1.5,3.6), (5, 2.5, 5.5), (10, 5, 11)]:
-            for threshold in [0, 0.17, 999]:
-                inFileList = [inputDir+"/MONO_"+str(nu_pdg)+"_"+str(e_mono[0])+"GeV_GENIEv3_G18_10a_00_000_1M_*_NUISFLAT.root",\
-                              inputDir+"/MONO_"+str(nu_pdg)+"_"+str(e_mono[0])+"GeV_GENIEv3_G18_10b_00_000_1M_*_NUISFLAT.root",\
-                              inputDir+"/MONO_"+str(nu_pdg)+"_"+str(e_mono[0])+"GeV_GENIEv3_G18_10c_00_000_1M_*_NUISFLAT.root",\
-                              inputDir+"/MONO_"+str(nu_pdg)+"_"+str(e_mono[0])+"GeV_GENIEv3_G18_10d_00_000_1M_*_NUISFLAT.root"\
-                              ]
-                enuhad = "ELep + Sum$((abs(pdg)==11 || (abs(pdg)>17 && abs(pdg)<2000))*(E > "+str(threshold)+")*E) + Sum$((abs(pdg)>2300 &&abs(pdg)<10000)*E) + Sum$((abs(pdg)==2212)*(E - sqrt(E*E - px*px - py*py - pz*pz)))"
-
-                
-                make_generator_comp("MONO_"+str(nu_pdg)+"_"+str(e_mono[0])+"GeV_Ar40_Enurec_FSIcomp_min"+str(threshold)+".png", inFileList, nameList, colzList, enuhad, \
-                                    "400,"+str(e_mono[1])+","+str(e_mono[2]), ehad_cut, "E_{#nu}^{rec, had} (GeV); d#sigma/dE_{#nu}^{rec, had} (#times 10^{-38} cm^{2}/nucleon)", False)
-                
-                
-                ## make_generator_comp(det+"_"+flux+"_Ar40_Enurecbias_FSIcomp_min"+str(threshold)+".png", \
-                ##                     inFileList, nameList, colzList, "("+enuhad+" - Enu_true)/Enu_true", "100,-0.3,0.2", ehad_cut, \
-                ##                     "(E_{#nu}^{rec, had} - E_{#nu}^{true})/E_{#nu}^{true}; Arb. norm.", True)
-                
-
-def make_W_plots(inputDir="inputs/"):
-
-    nameList = ["GENIE 10a",\
-                "GENIE 10b",\
-                "GENIE 10c",\
-                "CRPA",\
-                "SuSAv2",\
-                "NEUT",\
-                "NuWro"\
-                ]
-    colzList = [9000, 9001, 9002, 9003, 9004, 9006, 9005]
-    
-    ehad_cut = "cc==1"
-
-    ## Loop over configs
-    for det in ["T2KND", "T2KSK_osc", "DUNEND", "DUNEFD_osc"]:
-        for flux in ["FHC_numu", "RHC_numubar"]:
-
-            targ = "Ar40"
-            if "T2K" in det: targ = "H2O"
-            
-            ## These files can be found here (no login required): https://portal.nersc.gov/project/dune/data/2x2/simulation
-            inFileList = [inputDir+"/"+det+"_"+flux+"_"+targ+"_GENIEv3_G18_10a_00_000_1M_*_NUISFLAT.root",\
-                          inputDir+"/"+det+"_"+flux+"_"+targ+"_GENIEv3_G18_10b_00_000_1M_*_NUISFLAT.root",\
-                          inputDir+"/"+det+"_"+flux+"_"+targ+"_GENIEv3_G18_10c_00_000_1M_*_NUISFLAT.root",\
-                          inputDir+"/"+det+"_"+flux+"_"+targ+"_GENIEv3_CRPA21_04a_00_000_1M_*_NUISFLAT.root",\
-                          inputDir+"/"+det+"_"+flux+"_"+targ+"_GENIEv3_G21_11a_00_000_1M_*_NUISFLAT.root",\
-                          inputDir+"/"+det+"_"+flux+"_"+targ+"_NEUT562_1M_*_NUISFLAT.root",\
-                          inputDir+"/"+det+"_"+flux+"_"+targ+"_NUWRO_LFGRPA_1M_*_NUISFLAT.root"\
-                          ]
-            
-            make_generator_comp(det+"_"+flux+"_"+targ+"_W_gencomp.png", inFileList, nameList, colzList, "W", "200,0.5,3", ehad_cut, \
-                                "W (GeV); d#sigma/dW (#times 10^{-38} cm^{2}/nucleon)", False)
-            
 if __name__ == "__main__":
 
     inputDir="/global/cfs/cdirs/dune/users/cwilk/MC_IOP_review/*/"
-    # make_DUNE_FSI_erec_plots(inputDir)
-    # make_FSI_erec_plots(inputDir)
-    make_DUNE_erecoverq0_plots(inputDir)
-    # make_T2K_erec_plots(inputDir)
-    # make_DUNE_erec_plots(inputDir)
-    # make_W_plots(inputDir)
+    make_T2K_erec_plots(inputDir)
+    make_DUNE_erec_plots(inputDir)
+
