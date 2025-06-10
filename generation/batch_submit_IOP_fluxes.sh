ed3228df6d9d8a3ccba1a0e5dd2acb5fa7e028e0
Callum Wilkinson <cwilkinson@lbl.gov>
2025-06-10 03:26:47 -0700

    This script constantly evolves, but update to the current one anyway

--- generation/batch_submit_IOP_fluxes.sh
+++ generation/batch_submit_IOP_fluxes.sh
@@ -2,119 +2,131 @@
 
 ## If I want to pile more on later
 FIRST_JOB=0
-LAST_JOB=10
+LAST_JOB=9
 
-## Loop over these configs (max 23)
+## Configs to loop over
 FIRST_CONFIG=0
-LAST_CONFIG=23
+LAST_CONFIG=25
 
-## (Omit the last two here)
-
-name_arr=( "T2KSK_osc_FHC_numu_H2O" "T2KSK_osc_RHC_numubar_H2O" \
+name_arr=( "NOvA_osc_FHC_numu_CH2" "NOvA_osc_RHC_numubar_CH2" \
+	   "NOvA_unosc_FHC_numu_CH2" "NOvA_unosc_RHC_numubar_CH2" \
+	   "T2KSK_osc_FHC_numu_H2O" "T2KSK_osc_RHC_numubar_H2O" \
            "T2KSK_osc_FHC_numubar_H2O" "T2KSK_osc_RHC_numu_H2O" \
+	   "T2KSK_unosc_FHC_numu_H2O" "T2KSK_unosc_RHC_numubar_H2O" \
 	   "T2KND_FHC_numu_C8H8" "T2KND_RHC_numubar_C8H8" \
 	   "T2KND_FHC_numu_H2O" "T2KND_RHC_numubar_H2O" \
 	   "DUNEND_FHC_numu_Ar40" "DUNEND_RHC_numubar_Ar40" \
+	   "DUNEND_FHC_nue_Ar40" "DUNEND_RHC_nuebar_Ar40" \
 	   "DUNEFD_osc_FHC_numu_Ar40" "DUNEFD_osc_RHC_numubar_Ar40" \
-	   "flat_0-10GeV_numu_H2O" "flat_0-10GeV_numubar_H2O" \
-	   "flat_0-10GeV_numu_C8H8" "flat_0-10GeV_numubar_C8H8" \
-	   "flat_0-10GeV_numu_Ar40" "flat_0-10GeV_numubar_Ar40" \
-	   "flat_0-10GeV_nue_H2O" "flat_0-10GeV_nuebar_H2O" \
-           "flat_0-10GeV_nue_C8H8" "flat_0-10GeV_nuebar_C8H8" \
-           "flat_0-10GeV_nue_Ar40" "flat_0-10GeV_nuebar_Ar40" \
-	   "NuMIME_FHC_numu_Ar40" "NuMIME_RHC_numubar_Ar40" )
+	   "DUNEFD_unosc_FHC_numu_Ar40" "DUNEFD_unosc_RHC_numubar_Ar40" \
+	   "NuMIME_FHC_numu_Ar40" "NuMIME_RHC_numubar_Ar40" \
+	   "NuMIME_FHC_numu_C8H8" "NuMIME_RHC_numubar_C8H8" \
+	   "NuMILE_FHC_numu_C8H8" "NuMILE_RHC_numubar_C8H8" \
+	 )
 
 nu_pdg_arr=( 14 -14 \
-            -14  14 \
+	     14 -14 \
+	     14 -14 \
+	    -14  14 \
 	     14 -14 \
 	     14 -14 \
 	     14 -14 \
 	     14 -14 \
-	     14 -14 \
+             12 -12 \
 	     14 -14 \
              14 -14 \
-	     12 -12 \
-	     12 -12 \
-	     12 -12 \
+	     14 -14 \
+	     14 -14 \
 	     14 -14 \
 	   )
 
-emin_arr=( 0.1 0.15 \
-           0.15 0.1 \
-           0.1 0.15 \
-           0.1 0.15 \
+emin_arr=( 0.1 0.1 \
+	   0.1 0.1 \
+	   0.1 0.2 \
+           0.2 0.1 \
+	   0.1 0.2 \
+           0.1 0.2 \
+           0.1 0.2 \
            0.1 0.1 \
            0.1 0.1 \
+	   0.1 0.1 \
            0.1 0.1 \
            0.1 0.1 \
-           0.1 0.1 \
-           0.1 0.1 \
-           0.1 0.1 \
-           0.1 0.1 \
-           0.1 0.1 \
+	   0.1 0.1 \
+	   0.1 0.1 \
          )
 
-emax_arr=( 10 10 \
+emax_arr=( 20 20 \
+	   20 20 \
 	   10 10 \
-           10 10 \
+	   10 10 \
+	   10 10 \
+	   10 10 \
 	   10 10 \
 	   50 50 \
+           50 50 \
 	   50 50 \
-	   10 10 \
-	   10 10 \
-	   10 10 \
-	   10 10 \
-           10 10 \
-           10 10 \
+	   50 50 \
+	   50 50 \
+	   50 50 \
 	   50 50 \
 	 )
 
-targ_arr=( "1000080160[0.8889],1000010010[0.1111]" "1000080160[0.8889],1000010010[0.1111]" \
+targ_arr=( "1000060120[0.85714],1000010010[0.14286]" "1000060120[0.85714],1000010010[0.14286]" \
+	   "1000060120[0.85714],1000010010[0.14286]" "1000060120[0.85714],1000010010[0.14286]" \
+	   "1000080160[0.8889],1000010010[0.1111]" "1000080160[0.8889],1000010010[0.1111]" \
+           "1000080160[0.8889],1000010010[0.1111]" "1000080160[0.8889],1000010010[0.1111]" \
            "1000080160[0.8889],1000010010[0.1111]" "1000080160[0.8889],1000010010[0.1111]" \
 	   "1000060120[0.9231],1000010010[0.0769]" "1000060120[0.9231],1000010010[0.0769]" \
  	   "1000080160[0.8889],1000010010[0.1111]" "1000080160[0.8889],1000010010[0.1111]" \
 	   "1000180400[1.00]" "1000180400[1.00]" \
 	   "1000180400[1.00]" "1000180400[1.00]" \
-	   "1000080160[0.8889],1000010010[0.1111]" "1000080160[0.8889],1000010010[0.1111]" \
-           "1000060120[0.9231],1000010010[0.0769]" "1000060120[0.9231],1000010010[0.0769]" \
-	   "1000180400[1.00]" "1000180400[1.00]" \
-	   "1000080160[0.8889],1000010010[0.1111]" "1000080160[0.8889],1000010010[0.1111]" \
-           "1000060120[0.9231],1000010010[0.0769]" "1000060120[0.9231],1000010010[0.0769]" \
            "1000180400[1.00]" "1000180400[1.00]" \
-	   "1000180400[1.00]" "1000180400[1.00]" )
+	   "1000180400[1.00]" "1000180400[1.00]" \
+	   "1000180400[1.00]" "1000180400[1.00]" \
+	   "1000060120[0.9231],1000010010[0.0769]" "1000060120[0.9231],1000010010[0.0769]" \
+           "1000060120[0.9231],1000010010[0.0769]" "1000060120[0.9231],1000010010[0.0769]" \
+	 )
 
-flux_file_arr=( "T2KSK_FHC_NuFit5NO.root" "T2KSK_RHC_NuFit5NO.root" \
+flux_file_arr=( "NOvA_numu_osc_NuFit5NO.root" "NOvA_numubar_osc_NuFit5NO.root" \
+		"NOvA_numu_osc_NuFit5NO.root" "NOvA_numubar_osc_NuFit5NO.root" \
+		"T2KSK_FHC_NuFit5NO.root" "T2KSK_RHC_NuFit5NO.root" \
+		"T2KSK_FHC_NuFit5NO.root" "T2KSK_RHC_NuFit5NO.root" \
 		"T2KSK_FHC_NuFit5NO.root" "T2KSK_RHC_NuFit5NO.root" \
 		"t2kflux_2016_plus250kA.root" "t2kflux_2016_minus250kA.root" \
 		"t2kflux_2016_plus250kA.root" "t2kflux_2016_minus250kA.root" \
 		"DUNE_OptimizedEngineeredNov2017_REGULAR.root" "DUNE_OptimizedEngineeredNov2017_REGULAR.root" \
+		"DUNE_OptimizedEngineeredNov2017_REGULAR.root" "DUNE_OptimizedEngineeredNov2017_REGULAR.root" \
 		"DUNE_numu_osc_NuFit5NO.root" "DUNE_numu_osc_NuFit5NO.root" \
-		"flat_flux_10GeV.root" "flat_flux_10GeV.root" \
-		"flat_flux_10GeV.root" "flat_flux_10GeV.root" \
-		"flat_flux_10GeV.root" "flat_flux_10GeV.root" \
-		"flat_flux_10GeV.root" "flat_flux_10GeV.root" \
-                "flat_flux_10GeV.root" "flat_flux_10GeV.root" \
-                "flat_flux_10GeV.root" "flat_flux_10GeV.root" \
-		"MINERvA_flux_ME1F.root" "MINERvA_flux_ME5ARHC.root" )
+		"DUNE_numu_osc_NuFit5NO.root" "DUNE_numu_osc_NuFit5NO.root" \
+		"MINERvA_flux_ME1F.root" "MINERvA_flux_ME5ARHC.root" \
+		"MINERvA_flux_ME1F.root" "MINERvA_flux_ME5ARHC.root" \
+		"minerva_le_flux.root" "minerva_le_flux.root" \
+	      )
 
-flux_hist_arr=( "sk_numu_osc" "sk_numubar_osc" \
+flux_hist_arr=( "numu_osc" "numubar_osc" \
+		"numu" "numubar" \
+		"sk_numu_osc" "sk_numubar_osc" \
 		"sk_numubar_osc" "sk_numu_osc" \
+		"sk_numu_unosc" "sk_numubar_unosc" \
 		"enu_nd280_numu" "enu_nd280_numub" \
 		"enu_nd280_numu" "enu_nd280_numub" \
 		"numu_NDFHC_flux" "numubar_NDRHC_flux" \
+		"nue_NDFHC_flux" "nuebar_NDRHC_flux" \
 		"numu_FDFHC_flux_osc" "numubar_FDRHC_flux_osc" \
-		"flat_flux" "flat_flux" \
-	        "flat_flux" "flat_flux" \
-                "flat_flux" "flat_flux" \
-		"flat_flux" "flat_flux" \
-                "flat_flux" "flat_flux" \
-                "flat_flux" "flat_flux" \
-		"flux_E_cvweighted_CV_WithStatErr" "flux_E_cvweighted_CV_WithStatErr" )
+                "numu_FDFHC_flux" "numubar_FDRHC_flux" \
+		"flux_E_cvweighted_CV_WithStatErr" "flux_E_cvweighted_CV_WithStatErr" \
+		"flux_E_cvweighted_CV_WithStatErr" "flux_E_cvweighted_CV_WithStatErr" \
+		"numu_fhc" "numubar_rhc" \
+	      )
 
 ## Loop over templates
-for GENERATOR in GENIEv3_G18_10X_00_000 GENIEv3_G18_10a_00_000 GENIEv3_G18_10b_00_000 \
-		 GENIEv3_G18_10c_00_000 GENIEv3_CRPA21_04a_00_000 GENIEv3_G21_11a_00_000 \
-		 NUWRO_LFGRPA NUWRO_SF NEUT562 GENIEv3_AR23_20i_00_000
+# for GENERATOR in GiBUU
+for GENERATOR in NEUTDCC NUWROv25.3.1 \
+		 NEUT580 GENIEv3_G18_10a_00_000 GENIEv3_G18_10b_00_000 \
+		 GENIEv3_G18_10c_00_000 GENIEv3_G18_10d_00_000 \
+		 GENIEv3_CRPA21_04a_00_000 GENIEv3_G21_11a_00_000 \
+		 NUWRO_LFGRPA NEUT580 GENIEv3_AR23_20i_00_000
 do
     for i in $(seq ${FIRST_CONFIG} ${LAST_CONFIG}); do
 	
