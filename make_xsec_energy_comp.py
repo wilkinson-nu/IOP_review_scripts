1685d527a0ed30be5ad3a2cd8fbfc2b06ea7203a
Callum Wilkinson <cwilkinson@lbl.gov>
2025-11-26 10:58:35 -0800

    Various updates for near-final paper versions

--- make_xsec_energy_comp.py
+++ make_xsec_energy_comp.py
@@ -17,11 +17,15 @@ def make_xsec_energy_comp_plots(inputDir="inputs/", flav="numu", targ="Ar40", sa
     colzList = [8000, 8003, 8004, 8005, 8006, 8007, 8001]
     lineList = [1, 1, 1, 7, 1, 7, 1]
     
-    cut = "cc==1" ## && tgta != 1 && tgt != 1000010010" ## && Enu_true > 0.2"
+    cut = "cc==1 && Enu_true > 0.13 && nfsp > 0" ## && tgta != 1 && tgt != 1000010010" ## && Enu_true > 0.2"
     sample_label = "CCINC"
+    
+    binning = [0, 0.13, 0.15, 0.17, 0.19, 0.21, 0.23, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.7, 0.8, 0.9, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0, 4.2, 4.4, 4.6, 4.8, 5.0]
+
     if sample == "cc0pi":
         cut += "&& Sum$(abs(pdg) > 100 && abs(pdg) < 2000)==0 && Sum$(abs(pdg) > 2300 && abs(pdg) < 100000)==0"
         sample_label = "CC0#pi"
+        binning = [0, 0.13, 0.15, 0.17, 0.19, 0.21, 0.23, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 1.0, 1.2, 1.4, 1.8, 2.2, 2.6, 3.0, 3.4, 3.8, 4.2, 4.6, 5.0]
     if sample == "ccqe":
         cut += "&& abs(Mode)==1"
         sample_label = "CCQE"
@@ -29,8 +33,6 @@ def make_xsec_energy_comp_plots(inputDir="inputs/", flav="numu", targ="Ar40", sa
         cut += "&& abs(Mode)==2"
         sample_label = "2p2h"
 
-    binning = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0, 4.2, 4.4, 4.6, 4.8, 5.0]
-        
     ## Use the ensemble files
     inFileList = [inputDir+"/MONOENSEMBLE_"+flav+"_"+targ+"_*GeV_GENIEv3_G18_10a_00_000_100k_*_NUISFLAT.root",\
                   inputDir+"/MONOENSEMBLE_"+flav+"_"+targ+"_*GeV_GENIEv3_CRPA21_04a_00_000_100k_*_NUISFLAT.root",\
@@ -44,7 +46,7 @@ def make_xsec_energy_comp_plots(inputDir="inputs/", flav="numu", targ="Ar40", sa
     make_generator_comp("plots/XSEC_"+targ+"_"+flav+"_Enu_"+sample+"_gencomp.pdf", inFileList, \
                         nameList, colzList, lineList, "Enu_true", binning, cut, \
                         "E_{#nu}^{true} (GeV); #sigma(E_{#nu}^{true}) (#times 10^{-38} cm^{2}/nucleon)", \
-                        legDim=legDim, yRatLimits=[0.5, 2.2], norm="enu_ensemble")
+                        legDim=legDim, yRatLimits=[0.5, 2.2], norm="enu_ensemble", lineStyle="L")
     
 if __name__ == "__main__":
 
