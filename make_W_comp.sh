87927c4eb55131ff0c5234d1810582f0928b4ff8
Callum Wilkinson <cwilkinson@lbl.gov>
2026-05-16 15:47:07 -0700

    Make parallel versions of old scripts

---
+++ make_W_comp.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+INPUT_DIR="/pscratch/sd/c/cwilk/MC_IOP_review/\*/"
+PLOT_DIR="/global/homes/c/cwilk/IOP_review_scripts/new_plots"
+
+## List of plots I previously made sequentially
+arg_sets=(
+    "--input=${INPUT_DIR} --det=DUNEFD_osc --flux=FHC_numu --output=${PLOT_DIR}"
+    "--input=${INPUT_DIR} --det=DUNEFD_osc --flux=RHC_numubar --output=${PLOT_DIR}"
+)
+
+
+## Now submit them separately, one line at a time
+for i in "${!arg_sets[@]}"; do
+    args=${arg_sets[$i]}
+
+    echo $args
+
+    ## Construct a submission script
+    job_script=W_comp_script_${i}.sh
+    echo "#!/bin/bash" > ${job_script}
+    echo "#SBATCH --image=wilkinsonnu/nuisance_project:genie_v340" >> ${job_script}
+    echo "#SBATCH --qos=shared" >> ${job_script}
+    echo "#SBATCH --constraint=cpu" >> ${job_script}
+    echo "#SBATCH --time=180" >> ${job_script}
+    echo "#SBATCH --nodes=1" >> ${job_script}
+    echo "#SBATCH --ntasks=1" >> ${job_script}
+    echo "#SBATCH --mem=4GB" >> ${job_script}
+
+    echo "shifter --entrypoint python3 make_W_comp.py ${args}" >> ${job_script}
+    
+    ## Submit and $PROFIT
+    sbatch ${job_script}
+    rm ${job_script}    
+done
+
---
+++ make_q0_comp.py
@@ -0,0 +1,57 @@
+from plotting_functions import make_generator_comp, make_generator_ratio_comp
+import argparse
+
+def make_q0_comp(inputDir="inputs/", det="T2KND", flux="FHC_numu", outdir="plots/"):
+
+    nameList = ["GENIE 10a",\
+                "CRPA",\
+                "NEUT",\
+                "NEUT DCC",\
+                "NuWro 19",\
+                "NuWro 25",\
+                "GiBUU"\
+                ]
+    colzList = [8000, 8003, 8004, 8005, 8006, 8007, 8001]
+    lineList = [1, 1, 1, 7, 1, 7, 1]
+    
+    ehad_cut = "cc==1"
+
+    targ = "Ar40"
+    binning="60,0,3"
+    if "T2K" in det:
+        targ = "H2O"
+        binning="50,0,1"
+        
+    inFileList = [inputDir+"/"+det+"_"+flux+"_"+targ+"_GENIEv3_G18_10a_00_000_1M_*_NUISFLAT.root",\
+                  inputDir+"/"+det+"_"+flux+"_"+targ+"_GENIEv3_CRPA21_04a_00_000_1M_*_NUISFLAT.root",\
+                  inputDir+"/"+det+"_"+flux+"_"+targ+"_NEUT580_1M_*_NUISFLAT.root",\
+                  inputDir+"/"+det+"_"+flux+"_"+targ+"_NEUTDCC_1M_*_NUISFLAT.root",\
+                  inputDir+"/"+det+"_"+flux+"_"+targ+"_NUWRO_LFGRPA_1M_*_NUISFLAT.root",\
+                  inputDir+"/"+det+"_"+flux+"_"+targ+"_NUWROv25.3.1_1M_*_NUISFLAT.root",\
+                  inputDir+"/"+det+"_"+flux+"_"+targ+"_GiBUU_1M_*_NUISFLAT.root"\
+                  ]
+    
+    make_generator_comp(outdir+"/"+det+"_"+flux+"_"+targ+"_q0_gencomp.pdf", inFileList, nameList, colzList, lineList, "q0", binning, ehad_cut, \
+                        "q_{0} (GeV); d#sigma/dq_{0} (#times 10^{-38} cm^{2}/GeV/nucleon)", withRebin=True, legDim=[0.65, 0.44, 0.93, 0.93])
+        
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser("make_q0_comp")
+    
+    # Add arguments
+    parser.add_argument('--input',  type=str, required=True)
+    parser.add_argument('--output', type=str, required=True)
+    parser.add_argument('--det',    type=str, required=True)
+    parser.add_argument('--flux',   type=str, required=True)
+
+    ## Parse arguments from command line
+    args = parser.parse_args()
+
+    ## Report arguments
+    for arg in vars(args): print(arg, getattr(args, arg))
+    
+    make_q0_comp(args.input,
+                 args.det,
+                 args.flux,
+                 outdir=args.output)
+    
