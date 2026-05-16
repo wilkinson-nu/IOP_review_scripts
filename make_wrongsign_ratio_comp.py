9a16ed83bbda5d3b68d7ae6623f717c9bb13b912
Callum Wilkinson <cwilkinson@lbl.gov>
2026-05-16 13:17:54 -0700

    Minor fixes

--- make_wrongsign_ratio_comp.py
+++ make_wrongsign_ratio_comp.py
@@ -1,42 +1,11 @@
-import ROOT
-import os
-from ROOT import gStyle, TGaxis, TPad, TLine, gROOT, TH1, TColor, TCanvas, TFile, TH1D, gPad, TLegend, kWhite, gDirectory, gEnv
-from glob import glob
+from plotting_functions import make_A_over_BC_comp
+import argparse
 
-from plotting_functions import make_generator_ratio_comp, make_generator_comp, make_A_over_BC_comp
-
-## Use double precision for TTree draw
-gEnv.SetValue("Hist.Precision.1D", "double")
-
-## No need to see the plots appear here
-gROOT.SetBatch(1)
-gStyle.SetLineWidth(3)
-gStyle.SetOptStat(0)
-gStyle.SetOptTitle(0)
-gStyle.SetOptFit(0)
-TGaxis.SetMaxDigits(4)
-gStyle.SetLineStyleString(11,"40 20 40 20")
-gStyle.SetLineStyleString(12,"20 10 20 10")
-
-gStyle.SetTextSize(0.05)
-gStyle.SetLabelSize(0.05,"xyzt")
-gStyle.SetTitleSize(0.05,"xyzt")
-
-gStyle.SetPadTickX(1)
-gStyle.SetPadTickY(1)
-gStyle.SetNdivisions(505, "XY")
-
-gROOT .ForceStyle()
-
-TH1.SetDefaultSumw2()
-gStyle.SetLineWidth(3)
-
-## Sort out the position of the y axis exponent...
-TGaxis.SetExponentOffset(-0.06, 0., "y")
-
-def make_SK_wrongsign_ratio_plots(inputDir="inputs/", flav="numu"):
+def make_SK_wrongsign_ratio_plots(inputDir="inputs/", flav="numu", legDim=[0.2, 0.58, 0.75, 0.93], outdir="plots/"):
 
     nameList = ["GENIE 10a",\
+                "GENIE 10b",\
+                "GENIE 10c",\
                 "CRPA",\
                 "NEUT",\
                 "NEUT DCC",\
@@ -44,12 +13,11 @@ def make_SK_wrongsign_ratio_plots(inputDir="inputs/", flav="numu"):
                 "NuWro 25",\
                 "GiBUU"\
 		]
-    colzList = [8000, 8003, 8004, 8005, 8006, 8007, 8001]
-    lineList = [1, 1, 1, 7, 1, 7, 1]
+    colzList = [8000, 8008, 8002, 8003, 8004, 8005, 8006, 8007, 8001]
+    lineList = [1, 12, 7, 1, 1, 7, 1, 7, 1]
 
     ## QE reco
-    qe_cut = "cc==1 && Sum$(abs(pdg) > 100 && abs(pdg) < 2000)==0 && Sum$(abs(pdg) > 2300 && abs(pdg) < 100000)==0 && Sum$(pdg==-2212)==0 && Sum$(pdg==-2112)==0"
-
+    qe_cut = "cc==1 && nfsp>0 && Sum$(abs(pdg) > 100 && abs(pdg) < 2000)==0 && Sum$(abs(pdg) > 2300 && abs(pdg) < 100000)==0" # && Sum$(pdg==-2212)==0 && Sum$(pdg==-2112)==0"
 
     ## Change to Enu_QE to use a binding energy of 27 MeV! Not 34 like the NUISANCE default...                                                                                   
     binding = 27/1000.
@@ -62,14 +30,12 @@ def make_SK_wrongsign_ratio_plots(inputDir="inputs/", flav="numu"):
     mod_enuqe = "(2*("+str(m1)+"-"+str(binding)+")*ELep -"+str(ml)+"*"+str(ml)+" + "+str(m2)+"*"+str(m2)+" - ("+str(m1)+"-"+str(binding)+")*("+str(m1)+"-"+str(binding)+"))/" \
         +"(2*(("+str(m1)+"-"+str(binding)+") - ELep + sqrt(ELep*ELep - "+str(ml)+"*"+str(ml)+")*CosLep))"
 
-    # mod_enuqe = "Enu_QE"
-    
-    ## Allow hydrogen
-    # qe_cut += "&& tgta != 1 && tgt != 1000010010"
     sample_label = "CC0#pi"
 
     ## These plots need to be RHC numu / (RHC numu + RHC numubar)    
     inFileListNu = [inputDir+"/T2KSK_osc_RHC_"+flav+"_H2O_*GENIEv3_G18_10a_00_000_1M_*_NUISFLAT.root",\
+                    inputDir+"/T2KSK_osc_RHC_"+flav+"_H2O_*GENIEv3_G18_10b_00_000_1M_*_NUISFLAT.root",\
+                    inputDir+"/T2KSK_osc_RHC_"+flav+"_H2O_*GENIEv3_G18_10c_00_000_1M_*_NUISFLAT.root",\
                     inputDir+"/T2KSK_osc_RHC_"+flav+"_H2O_*GENIEv3_CRPA21_04a_00_000_1M_*_NUISFLAT.root",\
                     inputDir+"/T2KSK_osc_RHC_"+flav+"_H2O_*NEUT580_1M_*_NUISFLAT.root",\
                     inputDir+"/T2KSK_osc_RHC_"+flav+"_H2O_*NEUTDCC_1M_*_NUISFLAT.root",\
@@ -79,6 +45,8 @@ def make_SK_wrongsign_ratio_plots(inputDir="inputs/", flav="numu"):
                     ]
 
     inFileListNub = [inputDir+"/T2KSK_osc_RHC_"+flav+"bar_H2O_*GENIEv3_G18_10a_00_000_1M_*_NUISFLAT.root",\
+                     inputDir+"/T2KSK_osc_RHC_"+flav+"bar_H2O_*GENIEv3_G18_10b_00_000_1M_*_NUISFLAT.root",\
+                     inputDir+"/T2KSK_osc_RHC_"+flav+"bar_H2O_*GENIEv3_G18_10c_00_000_1M_*_NUISFLAT.root",\
                      inputDir+"/T2KSK_osc_RHC_"+flav+"bar_H2O_*GENIEv3_CRPA21_04a_00_000_1M_*_NUISFLAT.root",\
                      inputDir+"/T2KSK_osc_RHC_"+flav+"bar_H2O_*NEUT580_1M_*_NUISFLAT.root",\
                      inputDir+"/T2KSK_osc_RHC_"+flav+"bar_H2O_*NEUTDCC_1M_*_NUISFLAT.root",\
@@ -87,43 +55,32 @@ def make_SK_wrongsign_ratio_plots(inputDir="inputs/", flav="numu"):
                      inputDir+"/T2KSK_osc_RHC_"+flav+"bar_H2O_*GiBUU_1M_*_NUISFLAT.root"\
                      ]
 
-    ## make_generator_comp("plots/T2KSK_osc_wrongsign_"+flav+"_EnuQE_gencomp.pdf", inFileListNu, \
-    ##                     nameList, colzList, lineList, mod_enuqe, "40,0,2", qe_cut, \
-    ##                     "E_{#nu}^{rec, QE} (GeV); XSEC", lineStyle="", fluxAverage=False)
-    ## 
-    ## make_generator_comp("plots/T2KSK_osc_wrongsign_numubar_EnuQE_gencomp.pdf", inFileListNumub, \
-    ##                           nameList, colzList, lineList, mod_enuqe, "40,0,2", qe_cut, \
-    ##                           "E_{#nu}^{rec, QE} (GeV); XSEC", lineStyle="", fluxAverage=False)    
-
-    make_A_over_BC_comp("plots/T2KSK_osc_"+flav+"wrongsign_EnuQE_gencomp.pdf", inFileListNu, inFileListNub, inFileListNu, \
+    make_A_over_BC_comp(outdir+"/T2KSK_osc_"+flav+"wrongsign_EnuQE_gencomp.pdf", inFileListNu, inFileListNub, inFileListNu, \
                         nameList, colzList, lineList, mod_enuqe, "40,0,2", qe_cut, \
-                        "E_{#nu}^{rec, QE} (GeV); Wrong-sign/total", yLimits=[0,1.2], withRebin=True)
+                        "E_{#nu}^{rec, QE} (GeV); Wrong-sign/total", yLimits=[0,1.05], withRebin=True, legDim=legDim, legCols=2)
 
-    #make_A_over_BC_comp("plots/T2KSK_osc_wrongsign_EnuQE_neutron_gencomp.pdf", inFileListNu, inFileListNub, inFileListNu, \
-    #                    nameList, colzList, lineList, mod_enuqe, "40,0,2", qe_cut + "&& Sum$(pdg==2112)>0", \
-    #                    "E_{#nu}^{rec, QE} (GeV); Wrong-sign/total", yLimits=[0, 0.9], withRebin=True)
-
-
-def make_DUNE_wrongsign_ratio_plots(inputDir="inputs/", flav="numu", legDim=[0.65, 0.5, 0.85, 0.93]):
+def make_DUNE_wrongsign_ratio_plots(inputDir="inputs/", flav="numu", legDim=[0.4, 0.58, 0.95, 0.93], outdir="plots/"):
 
     nameList = ["GENIE 10a",\
+                "GENIE 10b",\
+                "GENIE 10c",\
                 "CRPA",\
                 "NEUT",\
                 "NEUT DCC",\
                 "NuWro 19",\
                 "NuWro 25",\
                 "GiBUU"\
-                ]
-    colzList = [8000, 8003, 8004, 8005, 8006, 8007, 8001]
-    lineList = [1, 1, 1, 7, 1, 7, 1]
+		]
+    colzList = [8000, 8008, 8002, 8003, 8004, 8005, 8006, 8007, 8001]
+    lineList = [1, 12, 7, 1, 1, 7, 1, 7, 1]
 
     ehad_cut = "cc==1 && nfsp > 0"
     enuhad = "ELep + Sum$((abs(pdg)==11 || (abs(pdg)>17 && abs(pdg)<2000))*E) + Sum$((abs(pdg)>2300 &&abs(pdg)<10000)*E) + \
     Sum$((abs(pdg)==2212)*(E - sqrt(E*E - px*px - py*py - pz*pz)))"
 
-
-
     inFileListNu = [inputDir+"/DUNEFD_osc_RHC_"+flav+"_Ar40_GENIEv3_G18_10a_00_000_1M_*_NUISFLAT.root",\
+                    inputDir+"/DUNEFD_osc_RHC_"+flav+"_Ar40_GENIEv3_G18_10b_00_000_1M_*_NUISFLAT.root",\
+                    inputDir+"/DUNEFD_osc_RHC_"+flav+"_Ar40_GENIEv3_G18_10c_00_000_1M_*_NUISFLAT.root",\
                     inputDir+"/DUNEFD_osc_RHC_"+flav+"_Ar40_GENIEv3_CRPA21_04a_00_000_1M_*_NUISFLAT.root",\
                     inputDir+"/DUNEFD_osc_RHC_"+flav+"_Ar40_NEUT580_1M_*_NUISFLAT.root",\
                     inputDir+"/DUNEFD_osc_RHC_"+flav+"_Ar40_NEUTDCC_1M_*_NUISFLAT.root",\
@@ -133,6 +90,8 @@ def make_DUNE_wrongsign_ratio_plots(inputDir="inputs/", flav="numu", legDim=[0.6
                     ]
 
     inFileListNub = [inputDir+"/DUNEFD_osc_RHC_"+flav+"bar_Ar40_GENIEv3_G18_10a_00_000_1M_*_NUISFLAT.root",\
+                     inputDir+"/DUNEFD_osc_RHC_"+flav+"bar_Ar40_GENIEv3_G18_10b_00_000_1M_*_NUISFLAT.root",\
+                     inputDir+"/DUNEFD_osc_RHC_"+flav+"bar_Ar40_GENIEv3_G18_10c_00_000_1M_*_NUISFLAT.root",\
                      inputDir+"/DUNEFD_osc_RHC_"+flav+"bar_Ar40_GENIEv3_CRPA21_04a_00_000_1M_*_NUISFLAT.root",\
                      inputDir+"/DUNEFD_osc_RHC_"+flav+"bar_Ar40_NEUT580_1M_*_NUISFLAT.root",\
                      inputDir+"/DUNEFD_osc_RHC_"+flav+"bar_Ar40_NEUTDCC_1M_*_NUISFLAT.root",\
@@ -141,17 +100,32 @@ def make_DUNE_wrongsign_ratio_plots(inputDir="inputs/", flav="numu", legDim=[0.6
                      inputDir+"/DUNEFD_osc_RHC_"+flav+"bar_Ar40_GiBUU_1M_*_NUISFLAT.root"\
                      ]
 
-    make_A_over_BC_comp("plots/DUNEFD_osc_"+flav+"wrongsign_Enurec_gencomp.pdf", inFileListNu, inFileListNub, inFileListNu, \
+    make_A_over_BC_comp(outdir+"/DUNEFD_osc_"+flav+"wrongsign_Enurec_gencomp.pdf", inFileListNu, inFileListNub, inFileListNu, \
                         nameList, colzList, lineList, enuhad, "80,0,8", ehad_cut, \
-                        "E_{#nu}^{rec, had} (GeV); Wrong-sign/total", yLimits=[0,0.9], withRebin=False, legDim=legDim)
+                        "E_{#nu}^{rec, had} (GeV); Wrong-sign/total", yLimits=[0,1.05], withRebin=True, legDim=legDim, legCols=2)
 
     
 if __name__ == "__main__":
 
-    inputDir="/pscratch/sd/c/cwilk/MC_IOP_review/*/"
+    parser = argparse.ArgumentParser("make_wrongsign_ratio_comp")
     
-    make_SK_wrongsign_ratio_plots(inputDir, "numu")
-    make_SK_wrongsign_ratio_plots(inputDir, "nue")
+    # Add arguments
+    parser.add_argument('--input',  type=str, required=True)
+    parser.add_argument('--output', type=str, required=True)
+    parser.add_argument('--det',    type=str, required=True)
+    parser.add_argument('--flav',   type=str, required=True)
 
-    make_DUNE_wrongsign_ratio_plots(inputDir, "numu", legDim=[0.25, 0.5, 0.45, 0.93])
-    make_DUNE_wrongsign_ratio_plots(inputDir, "nue", legDim=[0.65, 0.5, 0.85, 0.93])
+    ## Parse arguments from command line
+    args = parser.parse_args()
+
+    ## Report arguments
+    for arg in vars(args): print(arg, getattr(args, arg))
+
+    if "DUNE" in args.det:
+        make_DUNE_wrongsign_ratio_plots(args.input,
+                                        args.flav,
+                                        outdir=args.output)
+    else:
+        make_SK_wrongsign_ratio_plots(args.input,
+                                      args.flav,
+                                      outdir=args.output)
